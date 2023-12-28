5d38e0a3940f1bbcb9d65d8a49b79ec092e12148
Zenith <sqoulo11@gmail.com>
2023-12-28 18:35:30 +0700

    feat: add animation in contact page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,29 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideLeft: {
+          "0%": { transform: "translateX(-100%)" },
+          "70%": { transform: "translateX(2%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        slideRight: {
+          "0%": { transform: "translateX(100%)" },
+          "70%": { transform: "translateX(-2%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        "sliding-left": "slideLeft 1.5s",
+        "sliding-right": "slideRight 1.5s",
+      },
+    },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
   },
   plugins: [],
