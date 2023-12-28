439e2ff651c4e96642dca80bb879eb122babc3c9
Zenith <sqoulo11@gmail.com>
2023-12-28 22:49:55 +0700

    fix: try to add experimental assets: true

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  experimental: {
+    assets: true,
+  }
 });
