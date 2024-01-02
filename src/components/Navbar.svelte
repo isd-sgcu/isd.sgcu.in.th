141b42c77bdf3fbb72058ca405496fadd8a3496c
ChanatpakornD <csirintronsopon8@gmail.com>
2024-01-02 20:59:24 +0700

    feat: added animation

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,83 +8,88 @@
     
     const SGCU = "src/assets/navbar/logo-sgcu.svg"
     let MENU_HAMBURGER = "src/assets/navbar/menu-idle.svg"
-    let MENU_CLOSE = "src/assets/navbar/menu-close.svg"
-
-    function toggle(variable: boolean) {
-        variable = !variable;
-    }   
+    let MENU_CLOSE = "src/assets/navbar/menu-close.svg" 
+    let RIGHT_ARROW = "src/assets/navbar/rightArrow.png"
 
 </script>
 
-<nav class="fixed bg-gray-900 px-10 w-full text-white font-['Space Grotesk']">
+<nav id="nav" class="fixed bg-stone-950 px-4 lg:px-10 w-full text-white ">
     <div class="flex bg-blue justify-between py-4">
-        <img src={SGCU} alt="pic" class="h-24">
+        <img src={SGCU} alt="pic" class="h-8 lg:h-24">
         <div class="flex flex-row items-center">
-            <p class="mr-6 text-base font-light ">Menu</p>
-            <button on:click={() => toggle(open)}>
+            <p class="mr-2 lg:mr-6 text-base font-light font-['Space Grotesk']" >Menu</p>
+            <button on:click={() => {open = !open;}}>
                 {#if open}
-                    <img src={MENU_CLOSE} alt="pic" class="h-12" />
+                    <img src={MENU_CLOSE} alt="pic" class="h-8 lg:h-12" />
                 {:else}
-                    <img src={MENU_HAMBURGER} alt="pic" class="h-12"/>
+                    <img src={MENU_HAMBURGER} alt="pic" class="h-8 lg:h-12"/>
                 {/if}
             </button>
         </div>
     </div>
     
     {#if open}
-        <div class={"gap-10 flex flex-col h-screen text-9xl align-middle mt-12 animate-sliding-left"}>
-            <!-- svelte-ignore a11y-interactive-supports-focus -->
+        <div class={"mt-3 align-middle relative gap-10 flex flex-col min-h-screen px-4 text-4xl justify-start font-['JetBrains Mono'] font-light lg:mt-12 lg:text-9xl animate-sliding-left"}>
+            <!-- svelte-ignore a11y-no-static-element-interactions -->
             <div 
-            class="flex hover:ml-16"
-            role = "button"
+            class="cursor-pointer hover:ml-6 duration-700 "
             on:mouseenter={() => {hoverHome = true;}}
             on:mouseleave={() => {hoverHome = false;}}
-            >
-                <p class={hoverHome ? "block" : "hidden"} >-</p>
-                <p class={ (hoverHome) ? "-scale-x-100 " : ""}>H</p>
-                <p class={ (hoverHome) ? "-scale-x-100 " : ""}>O</p>
-                <p class={ (hoverHome) ? "-scale-x-100 " : ""}>M</p>
-                <p class={ (hoverHome) ? "-scale-x-100 " : ""}>E</p>
+            > 
+                <a href="/home" class="flex">
+                    <img src={RIGHT_ARROW} alt="" class={hoverHome ? "block" : "hidden"}>
+                    <p class={ (hoverHome) ? "-scale-x-100 duration-700" : ""}>H</p>
+                    <p class={ (hoverHome) ? "-scale-x-100 duration-700" : ""}>O</p>
+                    <p class={ (hoverHome) ? "-scale-x-100 duration-700" : ""}>M</p>
+                    <p class={ (hoverHome) ? "-scale-x-100 duration-700" : ""}>E</p>
+                </a>
+                
             </div>
             <!-- svelte-ignore a11y-no-static-element-interactions -->
             <div 
-            class="flex hover:ml-16"
+            class="flex cursor-pointer hover:ml-6 duration-700"
             on:mouseenter={() => {hoverProduct = true;}}
             on:mouseleave={() => {hoverProduct = false;}}
             >
-                <p class={hoverProduct ? "block" : "hidden"} >-</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>P</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>R</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>O</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>D</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>U</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>C</p>
-                <p class={ (hoverProduct) ? "-scale-x-100 " : ""}>T</p>
+            <a href="/project" class="flex">
+                <img src={RIGHT_ARROW} alt="" class={hoverProduct ? "block" : "hidden"}>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>P</p>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>R</p>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>O</p>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>J</p>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>E</p>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>C</p>
+                <p class={ (hoverProduct) ? "-scale-x-100 duration-700" : ""}>T</p>
+            </a>
             </div>
             <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <div class="flex hover:ml-16"
+            <div class="flex cursor-pointer hover:ml-6 duration-700"
             on:mouseenter={() => {hoverJoin = true;}}
             on:mouseleave={() => {hoverJoin = false;}}
             >
-                <p class={hoverJoin ? "block" : "hidden"} >-</p>
-                <p class={ (hoverJoin) ? "-scale-x-100":""}>J</p>
-                <p class={ (hoverJoin) ? "-scale-x-100":""}>O</p>
-                <p class={ (hoverJoin) ? "-scale-x-100":""}>I</p>
-                <p class={ (hoverJoin) ? "-scale-x-100":""}>N</p>
+            <a href="/join" class="flex">
+                <img src={RIGHT_ARROW} alt="" class={hoverJoin ? "block" : "hidden"}>
+                <p class={ (hoverJoin) ? "-scale-x-100 duration-700":""}>J</p>
+                <p class={ (hoverJoin) ? "-scale-x-100 duration-700":""}>O</p>
+                <p class={ (hoverJoin) ? "-scale-x-100 duration-700":""}>I</p>
+                <p class={ (hoverJoin) ? "-scale-x-100 duration-700":""}>N</p>
+            </a>
             </div>
             <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <div class="flex hover:ml-16"
+            <div class="flex cursor-pointer hover:ml-6 duration-700"
             on:mouseenter={() => {hoverContact = true;}}
             on:mouseleave={() => {hoverContact = false;}}
             >
-                <p class={hoverContact ? "block" : "hidden"} >-</p>
-                <p class={ (hoverContact) ? "-scale-x-100 " : ""}>C</p>
-                <p class={ (hoverContact) ? "-scale-x-100" : ""}>O</p>
-                <p class={ (hoverContact) ? "-scale-x-100" : ""}>N</p>
-                <p class={ (hoverContact) ? "-scale-x-100" : ""}>T</p>
-                <p class={ (hoverContact) ? "-scale-x-100" : ""}>A</p>
-                <p class={ (hoverContact) ? "-scale-x-100" : ""}>C</p>
-                <p class={ (hoverContact) ? "-scale-x-100" : ""}>T</p>
+                <a href="/contact" class="flex">
+                    <img src={RIGHT_ARROW} alt="" class={hoverContact ? "block" : "hidden"}>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>C</p>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>O</p>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>N</p>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>T</p>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>A</p>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>C</p>
+                    <p class={ (hoverContact) ? "-scale-x-100 duration-700" : ""}>T</p>
+                </a>
             </div>
             
         </div>  
