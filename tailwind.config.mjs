92ca2cb3d80d618fc9025e3a8c58a3aff5e96f8e
Thanaphom Hirunyathon <thanaphom.h@gmail.com>
2024-01-02 21:12:42 +0700

    Web 3 contact (#1)
    * feat: contact page
    
    * feat: add animation in contact page
    
    * refactor: pnpm format
    
    * style: contact header
    
    * refactor: pnpm format
    
    * fix: try to add experimental assets: true
    
    * fix: remove experimental
    
    * chore: add sharp
    
    * refactor: remove screens in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideLeft: {
+          "0%": { transform: "translateX(-100%)" },
+          "70%": { transform: "translateX(2%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        slideRight: {
+          "0%": { transform: "translateX(100%)" },
+          "70%": { transform: "translateX(-2%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        "sliding-left": "slideLeft 1.5s",
+        "sliding-right": "slideRight 1.5s",
+      },
+    },
   },
   plugins: [],
 };
