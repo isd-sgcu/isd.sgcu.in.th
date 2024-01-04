5e82362aaf742afc4aaa323a74312a4d9e77413f
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-01-04 22:28:47 +0700

    fix: project over navbar

--- src/components/project/ProjectContainer.svelte
+++ src/components/project/ProjectContainer.svelte
@@ -39,13 +39,13 @@
 <div>
   <div class="flex h-full flex-auto">
     <div
-      class="no-scrollbar mt-3 flex items-center gap-x-20 overflow-x-scroll overscroll-contain scroll-smooth whitespace-nowrap"
+      class="no-scrollbar mt-3 flex items-center gap-x-12 overflow-x-scroll overscroll-contain scroll-smooth whitespace-nowrap md:gap-x-20"
       bind:this={projectContainer}
       on:scroll={() => handleScroll()}
     >
       {#each projects as project, idx}
         <button
-          class={`w-fit shrink-0 ${idx == 0 ? "ml-[40px]" : ""} ${
+          class={`shrink-0 ${idx == 0 ? "ml-[40px]" : ""} ${
             idx == projects.length - 1 ? "mr-[40px]" : ""
           }`}
           on:click={() => {
@@ -56,8 +56,8 @@
             src={`/project/${project.image}`}
             alt={project.title}
             class={`duration-300 ease-in-out ${
-              idx % 2 == 0 ? "h-[45vh] md:h-[70vh]" : "h-[30vh] md:h-[40vh]"
-            } project`}
+              idx % 2 == 0 ? "h-[40vh] md:h-[60vh]" : " h-[25vh] md:h-[40vh]"
+            } project z-0`}
             on:mouseover={() => {
               hoverIndex = idx;
               applyHoverEffect(100, 0.95);
@@ -79,7 +79,7 @@
       {/each}
     </div>
   </div>
-  <div class="ml-[40px] mr-[40px] mt-10">
+  <div class="ml-[40px] mr-[40px] mt-5">
     <div class="flex h-1 w-full flex-row">
       <div class="h-1 bg-white" bind:this={progress}></div>
       <div class="h-1 w-full bg-gray-600" bind:this={remain}></div>
