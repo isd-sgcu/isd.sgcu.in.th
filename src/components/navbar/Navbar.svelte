1234c881412a4ee45ba325c0f0a526ad2e4fa977
HotPaotatoes <113345557+ChanatpakornD@users.noreply.github.com>
2024-01-11 14:57:48 +0700

    Chanatpakorn/web 6 navbar logo failed to load on production (#5)
    * feat: UI
    
    * feat: added animation
    
    * chore: format code
    
    * chore: refactor css
    
    * refactor: alt
    
    * refactor: format
    
    * refactor: pnpm format
    
    * feat: change color
    
    * refactor: color
    
    * fixed: all bugged & @ sv components
    
    ---------
    
    Co-authored-by: HotPaotatoes <113345557+ChanatpakornDev@users.noreply.github.com>
    Co-authored-by: Nutthapat Pongtanyavichai <59821765+leomotors@users.noreply.github.com>

--- src/components/navbar/Navbar.svelte
+++ src/components/navbar/Navbar.svelte
@@ -1,44 +1,40 @@
 <script lang="ts">
+  import Menu from "./Menu.svelte";
+  import LogoSgcu from "./LogoSGCU.svelte";
+  import RightArrow from "./RightArrow.svelte";
+
   let open = false;
   let hoverHome = false;
-  let hoverProduct = false;
+  let hoverProject = false;
   let hoverJoin = false;
   let hoverContact = false;
 
-  const SGCU = "src/assets/navbar/logo-sgcu.svg";
-  let MENU_HAMBURGER = "src/assets/navbar/menu-idle.svg";
-  let MENU_CLOSE = "src/assets/navbar/menu-close.svg";
-  let RIGHT_ARROW = "src/assets/navbar/rightArrow.png";
+  const RIGHT_ARROW = "./navbar/rightArrow.png";
 </script>
 
-<nav id="nav" class="fixed z-50 w-full bg-stone-950 px-4 text-white lg:px-10">
-  <div class="bg-blue flex justify-between py-4">
-    <img src={SGCU} alt="pic" class="h-8 lg:h-24" />
+<nav id="nav" class="z-50 w-full bg-primary px-4 text-white lg:px-10">
+  <div class="bg-blue flex items-center justify-between py-4">
+    <LogoSgcu
+      classProps="flex justify-start relative w-5 h-8 lg:h-24 lg:w-14"
+    />
     <div class="flex flex-row items-center">
-      <p class="font-['Space Grotesk'] mr-2 text-base font-light lg:mr-6">
-        Menu
-      </p>
+      <p class="mr-2 text-base font-light lg:mr-6">Menu</p>
       <button
         on:click={() => {
           open = !open;
         }}
       >
-        {#if open}
-          <img src={MENU_CLOSE} alt="" class="h-8 lg:h-12" />
-        {:else}
-          <img src={MENU_HAMBURGER} alt="" class="h-8 lg:h-12" />
-        {/if}
+        <Menu classProps="h-8 lg:h-12" isOpen={open} />
       </button>
     </div>
   </div>
 
   {#if open}
     <div
-      class="font-['JetBrains Mono'] transfr relative mt-[33.33%] flex min-h-screen animate-sliding-left flex-col gap-10 px-4 align-middle text-4xl font-light lg:mt-12 lg:justify-start lg:text-9xl"
+      class="transfrom relative mt-[30%] flex min-h-screen animate-sliding-left flex-col gap-10 px-4 align-middle text-4xl font-light lg:mt-12 lg:justify-start lg:text-9xl"
     >
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="menu-container hover:ml-7"
         on:mouseenter={() => {
           hoverHome = true;
         }}
@@ -46,11 +42,14 @@
           hoverHome = false;
         }}
       >
-        <a href="/" class="flex {hoverHome ? 'hover-menu' : ''}">
-          <img
-            src={RIGHT_ARROW}
-            alt=""
-            class="h-9 lg:h-32 {hoverHome ? 'block' : 'hidden'}"
+        <a
+          href="/"
+          class="menu-container {hoverHome ? 'hover-menu hover:ml-4' : ''}"
+        >
+          <RightArrow
+            classProps="h-9 w-9 lg:h-32 lg:w-32 {hoverHome
+              ? 'block'
+              : 'hidden'}"
           />
           <p>H</p>
           <p>O</p>
@@ -60,19 +59,21 @@
       </div>
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="menu-container hover:ml-6"
         on:mouseenter={() => {
-          hoverProduct = true;
+          hoverProject = true;
         }}
         on:mouseleave={() => {
-          hoverProduct = false;
+          hoverProject = false;
         }}
       >
-        <a href="/project" class="flex {hoverProduct ? 'hover-menu' : ''}">
-          <img
-            src={RIGHT_ARROW}
-            alt=""
-            class="h-9 lg:h-32 {hoverProduct ? 'block' : 'hidden'}"
+        <a
+          href="/project"
+          class="menu-container {hoverProject ? 'hover-menu hover:ml-4' : ''}"
+        >
+          <RightArrow
+            classProps="h-9 w-9 lg:h-32 lg:w-32 {hoverProject
+              ? 'block'
+              : 'hidden'}"
           />
           <p>P</p>
           <p>R</p>
@@ -85,7 +86,6 @@
       </div>
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="menu-container hover:ml-6"
         on:mouseenter={() => {
           hoverJoin = true;
         }}
@@ -93,11 +93,14 @@
           hoverJoin = false;
         }}
       >
-        <a href="/join" class="flex {hoverJoin ? 'hover-menu' : ''}">
-          <img
-            src={RIGHT_ARROW}
-            alt=""
-            class="h-9 lg:h-32 {hoverJoin ? 'block' : 'hidden'}"
+        <a
+          href="/join"
+          class="menu-container {hoverJoin ? 'hover-menu hover:ml-4' : ''}"
+        >
+          <RightArrow
+            classProps="h-9 w-9 lg:h-32 lg:w-32 {hoverJoin
+              ? 'block'
+              : 'hidden'}"
           />
           <p>J</p>
           <p>O</p>
@@ -107,7 +110,6 @@
       </div>
       <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
-        class="menu-container hover:ml-6"
         on:mouseenter={() => {
           hoverContact = true;
         }}
@@ -115,11 +117,14 @@
           hoverContact = false;
         }}
       >
-        <a href="/contact" class="flex {hoverContact ? 'hover-menu' : ''} ">
-          <img
-            src={RIGHT_ARROW}
-            alt=""
-            class="h-9 lg:h-32 {hoverContact ? 'block' : 'hidden'}"
+        <a
+          href="/contact"
+          class="menu-container {hoverContact ? 'hover-menu hover:ml-4' : ''}"
+        >
+          <RightArrow
+            classProps="h-9 w-9 lg:h-32 lg:w-32 {hoverContact
+              ? 'block'
+              : 'hidden'}"
           />
           <p>C</p>
           <p>O</p>
