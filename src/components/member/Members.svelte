5ff868fabcf49428f39a803d847812ac63a3ede8
ChanatpakornD <csirintronsopon8@gmail.com>
2024-01-29 20:41:54 +0700

    refactor: camel case

--- src/components/member/Members.svelte
+++ src/components/member/Members.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import ProfileCard from "./ProfileCard.svelte";
+  import RightTick from "./RightTick.svelte";
   import type { OrgMember } from "./types.ts";
-  export let OrgMembers: OrgMember[];
+  export let members: OrgMember[];
 </script>
 
 <section>
@@ -12,10 +13,10 @@
       <p class="text-4xl font-medium text-white">Members</p>
       <div class="mt-6 flex w-full flex-col justify-center lg:my-20">
         <div class="inline-flex flex-wrap justify-center">
-          {#if !OrgMembers || OrgMembers.length === 0}
+          {#if !members || members.length === 0}
             <p>No Public Members found...</p>
           {:else}
-            {#each OrgMembers as member}
+            {#each members as member}
               <ProfileCard
                 username={member.login}
                 avatarURL={member.avatar_url}
@@ -31,6 +32,7 @@
         class="item-center rounded-lg border border-[#807F7E] bg-transparent px-4 py-2 font-normal text-[#807F7E] duration-100 hover:bg-[#807F7E] hover:text-primary"
       >
         All members
+        <RightTick classProps="inline-block" />
       </button>
     </a>
   </div>
