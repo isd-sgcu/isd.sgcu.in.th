db5c7aaa22ecb5e96125bbb0e391b20b81c4974c
ChanatpakornD <csirintronsopon8@gmail.com>
2024-01-29 16:38:53 +0700

    feat: members

--- src/components/member/Members.svelte
+++ src/components/member/Members.svelte
@@ -5,22 +5,33 @@
 </script>
 
 <section>
-  <div class="container bg-blue-500">
-    <p class="text-4xl font-bold text-white">Members</p>
-    <div class="inline-flex items-center justify-center">
-      <div class="profile-container">
-        {#if !OrgMembers ||OrgMembers.length === 0}
-          <p>No Public Members found...</p>
-        {:else}
-          {#each OrgMembers as member}
-            <ProfileCard
-              username={member.login}
-              avatarURL={member.avatar_url}
-              profileURL={member.html_url}
-            />
-          {/each}
-        {/if}
+  <div
+    class="mb-20 flex flex-col items-center justify-center gap-4 px-8 text-center"
+  >
+    <div class="w-3/4">
+      <p class="text-4xl font-medium text-white">Members</p>
+      <div class="mt-6 flex w-full flex-col justify-center lg:my-20">
+        <div class="inline-flex flex-wrap justify-center">
+          {#if !OrgMembers || OrgMembers.length === 0}
+            <p>No Public Members found...</p>
+          {:else}
+            {#each OrgMembers as member}
+              <ProfileCard
+                username={member.login}
+                avatarURL={member.avatar_url}
+                profileURL={member.html_url}
+              />
+            {/each}
+          {/if}
+        </div>
       </div>
     </div>
+    <a href="https://github.com/orgs/isd-sgcu/people">
+      <button
+        class="item-center rounded-lg border border-[#807F7E] bg-transparent px-4 py-2 font-normal text-[#807F7E] duration-100 hover:bg-[#807F7E] hover:text-primary"
+      >
+        All members
+      </button>
+    </a>
   </div>
 </section>
--- src/components/member/ProfileCard.svelte
+++ src/components/member/ProfileCard.svelte
@@ -5,14 +5,19 @@
 </script>
 
 <div>
-  <div class="m-2 inline-block">
+  <div class="mx-2 my-4 inline-block">
     <a class="no-underline" title={username} href={profileURL}>
       <div
         id="profile"
-        class="rounded bg-[#232428] p-2 text-white hover:scale-110"
+        class="rounded bg-[#232428] p-2 text-white duration-200 hover:scale-110"
       >
         <div class="flex flex-row items-center">
-          <img alt={username} class="mr-3 rounded-full" src={avatarURL} />
+          <img
+            id="image-profile"
+            alt={username}
+            class="mr-2 h-8 w-8 rounded-full"
+            src={avatarURL}
+          />
           <p class="text-sm">
             @ {username}
           </p>
@@ -21,3 +26,10 @@
     </a>
   </div>
 </div>
+
+<style lang="post-css">
+  #image-profile {
+    width: 30px;
+    height: 30px;
+  }
+</style>
